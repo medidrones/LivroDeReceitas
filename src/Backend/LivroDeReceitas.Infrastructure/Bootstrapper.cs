5ac27c85f68de30b07ec72bc0a00b3583a5a9589
Jorge Medina <medidrones@gmail.com>
2023-05-25 14:36:18 -0300

    Adicionando as classes de migration da tabela usuario no banco de dados

---
+++ src/Backend/LivroDeReceitas.Infrastructure/Bootstrapper.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using FluentMigrator.Runner;
+using LivroDeReceitas.Domain.Extensions;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LivroDeReceitas.Infrastructure;
+
+public static class Bootstrapper
+{
+    public static void AddRepositorio(this IServiceCollection services, IConfiguration configurationManager)
+    {
+        AddFluentMigrator(services, configurationManager);
+    }
+
+    private static void AddFluentMigrator(IServiceCollection services, IConfiguration configurationManager)
+    {
+        services.AddFluentMigratorCore().ConfigureRunner(c => 
+            c.AddMySql5()
+                .WithGlobalConnectionString(configurationManager.GetConexaoCompleta()).ScanIn(Assembly.Load("LivroDeReceitas.Infrastructure"))
+                .For.All());
+    }
+}
---
+++ src/Backend/LivroDeReceitas.Infrastructure/Migrations/VersaoBase.cs
@@ -0,0 +1,13 @@
+﻿using FluentMigrator.Builders.Create.Table;
+
+namespace LivroDeReceitas.Infrastructure.Migrations;
+
+public static class VersaoBase
+{
+    public static ICreateTableColumnOptionOrWithColumnSyntax InserirColunasPadrao(ICreateTableWithColumnOrSchemaOrDescriptionSyntax tabela)
+    {
+        return tabela
+            .WithColumn("id").AsInt64().PrimaryKey().Identity()
+            .WithColumn("DataCriacao").AsDateTime().NotNullable();
+    }
+}
