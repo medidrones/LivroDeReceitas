e2f1ef97023cab9354e7f95e6d7e65415fc8cc7b
Jorge Medina <medidrones@gmail.com>
2023-05-27 17:42:30 -0300

    Criando testes de unidade para o validator de registrar usuario

---
+++ tests/Utilitario.Test/Requisicoes/RequisicaoRegistrarUsuarioBuilder.cs
@@ -0,0 +1,16 @@
+﻿using Bogus;
+using LivroDeReceitas.Comunicacao.Requisicoes;
+
+namespace Utilitario.Test.Requisicoes;
+
+public class RequisicaoRegistrarUsuarioBuilder
+{
+    public static RequisicaoRegistrarUsuarioJson Construir(int tamanhoSenha = 10)
+    {
+        return new Faker<RequisicaoRegistrarUsuarioJson>()
+            .RuleFor(c => c.Nome, f => f.Person.FullName)
+            .RuleFor(c => c.Email, f => f.Internet.Email())
+            .RuleFor(c => c.Senha, f => f.Internet.Password(tamanhoSenha))
+            .RuleFor(c => c.Telefone, f => f.Phone.PhoneNumber("## ! ####-####").Replace("!", $"{f.Random.Int(min: 1, max: 9)}"));
+    }
+}
---
+++ tests/Validators.Test/Usuario/Registrar/RegistrarUsuarioValidatorTest.cs
@@ -0,0 +1,124 @@
+﻿using FluentAssertions;
+using LivroDeReceitas.Application.UseCases.Usuario.Registrar;
+using LivroDeReceitas.Exceptions;
+using Utilitario.Test.Requisicoes;
+using Xunit;
+
+namespace Validators.Test.Usuario.Registrar;
+
+public class RegistrarUsuarioValidatorTest
+{
+    [Fact]
+    public void Validar_Sucesso()
+    {
+        var validator = new RegistrarUsuarioValidator();
+
+        var requisicao = RequisicaoRegistrarUsuarioBuilder.Construir();
+
+        var resultado = validator.Validate(requisicao);
+
+        resultado.IsValid.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Validar_Erro_Nome_Vazio()
+    {
+        var validator = new RegistrarUsuarioValidator();
+
+        var requisicao = RequisicaoRegistrarUsuarioBuilder.Construir();
+        requisicao.Nome = string.Empty;
+
+        var resultado = validator.Validate(requisicao);
+
+        resultado.IsValid.Should().BeFalse();
+        resultado.Errors.Should().ContainSingle().And.Contain(error => error.ErrorMessage.Equals(ResourceMensagensDeErro.NOME_USUARIO_EM_BRANCO));
+    }
+
+    [Fact]
+    public void Validar_Erro_Email_Vazio()
+    {
+        var validator = new RegistrarUsuarioValidator();
+
+        var requisicao = RequisicaoRegistrarUsuarioBuilder.Construir();
+        requisicao.Email = string.Empty;
+
+        var resultado = validator.Validate(requisicao);
+
+        resultado.IsValid.Should().BeFalse();
+        resultado.Errors.Should().ContainSingle().And.Contain(error => error.ErrorMessage.Equals(ResourceMensagensDeErro.EMAIL_USUARIO_EM_BRANCO));
+    }
+
+    [Fact]
+    public void Validar_Erro_Senha_Vazio()
+    {
+        var validator = new RegistrarUsuarioValidator();
+
+        var requisicao = RequisicaoRegistrarUsuarioBuilder.Construir();
+        requisicao.Senha = string.Empty;
+
+        var resultado = validator.Validate(requisicao);
+
+        resultado.IsValid.Should().BeFalse();
+        resultado.Errors.Should().ContainSingle().And.Contain(error => error.ErrorMessage.Equals(ResourceMensagensDeErro.SENHA_USUARIO_EM_BRANCO));
+    }
+
+    [Fact]
+    public void Validar_Erro_Telefone_Vazio()
+    {
+        var validator = new RegistrarUsuarioValidator();
+
+        var requisicao = RequisicaoRegistrarUsuarioBuilder.Construir();
+        requisicao.Telefone = string.Empty;
+
+        var resultado = validator.Validate(requisicao);
+
+        resultado.IsValid.Should().BeFalse();
+        resultado.Errors.Should().ContainSingle().And.Contain(error => error.ErrorMessage.Equals(ResourceMensagensDeErro.TELEFONE_USUARIO_EM_BRANCO));
+    }
+
+    [Fact]
+    public void Validar_Erro_Email_Invalido()
+    {
+        var validator = new RegistrarUsuarioValidator();
+
+        var requisicao = RequisicaoRegistrarUsuarioBuilder.Construir();
+        requisicao.Email = "med";
+
+        var resultado = validator.Validate(requisicao);
+
+        resultado.IsValid.Should().BeFalse();
+        resultado.Errors.Should().ContainSingle().And.Contain(error => error.ErrorMessage.Equals(ResourceMensagensDeErro.EMAIL_USUARIO_INVALIDO));
+    }
+
+    [Fact]
+    public void Validar_Erro_Telefone_Invalido()
+    {
+        var validator = new RegistrarUsuarioValidator();
+
+        var requisicao = RequisicaoRegistrarUsuarioBuilder.Construir();
+        requisicao.Telefone = "11 9";
+
+        var resultado = validator.Validate(requisicao);
+
+        resultado.IsValid.Should().BeFalse();
+        resultado.Errors.Should().ContainSingle().And.Contain(error => error.ErrorMessage.Equals(ResourceMensagensDeErro.TELEFONE_USUARIO_INVALIDO));
+    }
+
+    [Theory]
+    [InlineData(1)]
+    [InlineData(2)]
+    [InlineData(3)]
+    [InlineData(4)]
+    [InlineData(5)]
+    public void Validar_Erro_Senha_Invalida(int tamanhoSenha)
+    {
+        var validator = new RegistrarUsuarioValidator();
+
+        var requisicao = RequisicaoRegistrarUsuarioBuilder.Construir(tamanhoSenha);
+
+        var resultado = validator.Validate(requisicao);
+
+        resultado.IsValid.Should().BeFalse();
+        resultado.Errors.Should().ContainSingle().And.Contain(error => error.ErrorMessage.Equals(ResourceMensagensDeErro.SENHA_USUARIO_MINIMO_SEIS_CARACTERES));
+    }
+}
