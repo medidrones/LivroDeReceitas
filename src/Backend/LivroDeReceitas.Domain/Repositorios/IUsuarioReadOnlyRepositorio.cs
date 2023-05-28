17f219c2803595da104840c48e64e0ec3b1dc24a
Jorge Medina <medidrones@gmail.com>
2023-05-28 01:20:08 -0300

    Adicionando função no repositorio para recuperar o usuario fazendo login

--- src/Backend/LivroDeReceitas.Domain/Repositorios/IUsuarioReadOnlyRepositorio.cs
+++ src/Backend/LivroDeReceitas.Domain/Repositorios/IUsuarioReadOnlyRepositorio.cs
@@ -1,6 +1,9 @@
-﻿namespace LivroDeReceitas.Domain.Repositorios;
+﻿using LivroDeReceitas.Domain.Entidades;
+
+namespace LivroDeReceitas.Domain.Repositorios;
 
 public interface IUsuarioReadOnlyRepositorio
 {
     Task<bool> ExisteUsuarioComEmail(string email);
+    Task<Usuario> Login(string email, string senha);
 }
