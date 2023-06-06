7d1e7feef5f1c2ec6e7ef4199aa26e81a2708524
Jorge Medina <medidrones@gmail.com>
2023-06-05 22:55:07 -0300

    Feat: Adicionado EndPoint para recuperar as receitas do usuario

---
+++ src/Backend/LivroDeReceitas.Api/Controllers/DashboardController.cs
@@ -0,0 +1,28 @@
+﻿using LivroDeReceitas.Api.Filtros;
+using LivroDeReceitas.Application.UseCases.Dashboard;
+using LivroDeReceitas.Comunicacao.Requisicoes;
+using LivroDeReceitas.Comunicacao.Respostas;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LivroDeReceitas.Api.Controllers;
+
+public class DashboardController : LivroDeReceitasController
+{
+    [HttpPut]
+    [ProducesResponseType(typeof(RespostaDashboardJson), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ServiceFilter(typeof(UsuarioAutenticadoAttribute))]
+    public async Task<IActionResult> RecuperarDashboard(
+        [FromServices] IDashboardUseCase useCase,
+        [FromBody] RequisicaoDashboardJson request)
+    {
+        var resultado = await useCase.Executar(request);
+
+        if (resultado.Receitas.Any())
+        {
+            return Ok(resultado);
+        }
+
+        return NoContent();
+    }
+}
--- src/Backend/LivroDeReceitas.Infrastructure/AcessoRepositorio/Repositorio/ReceitaRepositorio.cs
+++ src/Backend/LivroDeReceitas.Infrastructure/AcessoRepositorio/Repositorio/ReceitaRepositorio.cs
@@ -20,6 +20,8 @@ public class ReceitaRepositorio : IReceitaWriteOnlyRepositorio, IReceitaReadOnly
 
     public async Task<IList<Receita>> RecuperarTodasDoUsuario(long usuarioId)
     {
-        return await _contexto.Receitas.Where(r => r.UsuarioId == usuarioId).ToListAsync();
+        return await _contexto.Receitas.AsNoTracking()
+            .Include(r => r.Ingredientes)
+            .Where(r => r.UsuarioId == usuarioId).ToListAsync();
     }
 }
