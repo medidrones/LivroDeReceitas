d4fc214233a90e87b3a1db0266ed2567ac28c3fe
Jorge Medina <medidrones@gmail.com>
2023-06-07 19:37:24 -0300

    Feat: Adicionaro regra de negócio para atualizar a receita

---
+++ src/Backend/LivroDeReceitas.Application/UseCases/Receita/Atualizar/AtualizarReceitaValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+using LivroDeReceitas.Comunicacao.Requisicoes;
+
+namespace LivroDeReceitas.Application.UseCases.Receita.Atualizar;
+
+public class AtualizarReceitaValidator : AbstractValidator<RequisicaoReceitaJson>
+{
+    public AtualizarReceitaValidator()
+    {
+        RuleFor(x => x).SetValidator(new ReceitaValidator());
+    }
+}
